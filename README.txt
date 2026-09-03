06db47a739e6ad093b60ea3969e808e0f40e12dc
safayildiz99-netizen <safayildiz99@gmail.com>
2026-09-03 14:10:30 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+StarSnap Upload Ready
+
+Login für StarSnap UND Family Center:
+Benutzername: admin
+Passwort: 1234
+
+Wichtig: Öffne index.html. Keine Backup-Datei verwenden.
+Für Kamera/Face-Tracking auf HTTPS (z.B. Vercel) hochladen.
---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='starsnap-v5-ar-ai';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request)))});
